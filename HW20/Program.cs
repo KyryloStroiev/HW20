b1872bd6c8a5281309dd89b2e2198601ae31daad
KyryloStroiev <stroievkyrylo@gmail.com>
2022-12-09 18:10:51 +0200

    Update Program.cs

--- HW20/Program.cs
+++ HW20/Program.cs
@@ -4,15 +4,7 @@ using System.Text.Json;
 
 var bookMeeting = new BookMeeting();
 bookMeeting.TextMeeting();
+var serializer = new Serializer();
+serializer.SerializeMeeting("Using.json");
+serializer.Deserialize("Using.json");
 
-using var fileStream = new FileStream("User.json", FileMode.OpenOrCreate);
-{
-    var meeting = bookMeeting;
-    JsonSerializer.Serialize<BookMeeting>(fileStream, meeting);
-
-}
-using var fileStream1 = new FileStream("User.json", FileMode.OpenOrCreate);
-{
-    var meetings = JsonSerializer.Deserialize<BookMeeting>(fileStream1);
-    Console.WriteLine($"{meetings.DateMeeting} {meetings.ThemeMeeting}");
-}
