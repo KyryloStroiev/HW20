b7f14b891bfd42631f4dab12806b927203f40a34
KyryloStroiev <stroievkyrylo@gmail.com>
2022-12-12 16:02:55 +0200

    Update Program.cs

--- HW20/Program.cs
+++ HW20/Program.cs
@@ -3,8 +3,28 @@ using System;
 using System.Text.Json;
 
 var bookMeeting = new BookMeeting();
-bookMeeting.TextMeeting();
+
 var serializer = new Serializer();
-serializer.SerializeMeeting("Using.json");
-serializer.Deserialize("Using.json");
+ 
+    Console.WriteLine("Для запису нової зустрiчi натиснiть \"R\" Для перегляду записiв натисніть \"W\" ");
+    var entercod = Convert.ToChar(Console.ReadLine());
+    if(entercod == 'R')
+    {
+    bookMeeting.TextMeeting();
+    serializer.SerializeMeeting("Using.json");
+    }
+    else if (entercod == 'W')
+    {
+        
+
+        serializer.Deserialize("Using.json");
+    }
+    else
+    {
+        Console.WriteLine("Ви вели неправильний символ");
+        
+    }
+ 
+
+
 
